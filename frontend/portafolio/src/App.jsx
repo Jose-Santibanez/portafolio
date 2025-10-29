5986899edd09fe35e98b07c153907afc299eb189
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-10-28 22:16:13 -0300

    subiendo creación de ficheros de componentes: Home, Proyectos, Skills

--- frontend/portafolio/src/App.jsx
+++ frontend/portafolio/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 //import { getUsuario } from './services/api.js'
 import './App.css'
-import { useAxios } from './hooks/useFetchData.js'
+//import { useAxios } from './hooks/useFetchData.js'
 
 function App() {
-const {data:user, loading, error } = useAxios('/usuario/1') 
+/* const {data:user, loading, error } = useAxios('/usuario/1') 
 
   if(loading) return <p>Cargando Perfil...</p>;
-  if(error) return <p>Error: {error}</p>
+  if(error) return <p>Error: {error}</p> */
 
-console.log(user)
-  
+
   return (
     <>
       <div>
