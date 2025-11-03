9e3be107a6c207876864b293f82d6e34cdf9ab44
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-02 23:02:42 -0300

    seccion habilidades: fetching de datos

---
+++ frontend/portafolio/src/components/common/Habilidad.jsx
@@ -0,0 +1,14 @@
+import Habilidades from "../sections/Habilidades"
+
+
+const Habilidad = ()=>{
+    
+    return(
+        <div className="skill-icon reveal">
+            <img src="" alt="" />
+            <p></p>
+        </div>
+    )  
+}
+
+export default Habilidades
--- frontend/portafolio/src/components/sections/Habilidades.jsx
+++ frontend/portafolio/src/components/sections/Habilidades.jsx
@@ -1,11 +1,14 @@
+import { useAxios } from "../../hooks/useFetchData"
 
 const Habilidades = () =>{
-
+    const {data,loading,error} = useAxios('/habilidades')
+    if(loading) return <p>Cargando Perfil</p>
+    if(error) return <p>Error: {error}</p>
     return(
         <section className="skills-section">
             <h2>Habilidades</h2>
             <div className="skills-icons">
-
+                
             </div>
         </section>
     )
