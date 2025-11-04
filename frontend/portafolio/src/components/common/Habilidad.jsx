d453234df305812add8fb620c6d4fe9212437755
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-03 21:54:08 -0300

    seccion habilidad: se agregar columnna imagne y se insertan cdn en  la BD

--- frontend/portafolio/src/components/common/Habilidad.jsx
+++ frontend/portafolio/src/components/common/Habilidad.jsx
@@ -3,7 +3,7 @@ const Habilidad = ({props})=>{
     
     return(
         <div className="skill-icon reveal">
-            <img src="" alt="" />
+            <img src={props.imagen} alt={props.nombre} />
             <p>{props.nombre}</p>
         </div>
     )  
