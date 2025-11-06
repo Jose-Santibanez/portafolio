3dc309fcefb0b2a90035c3b945f2d330b9a513ce
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-06 20:54:03 -0300

    componente experiencias: Se crea contenedor y se realiza el fetch de datos

--- frontend/portafolio/src/components/sections/Experiencias.jsx
+++ frontend/portafolio/src/components/sections/Experiencias.jsx
@@ -1,8 +1,21 @@
+import { useAxios } from "../../hooks/useFetchData"
 
 
 const Experiencias = () =>{
 
-    return
+    const {data:experiences, loading, error} = useAxios('/experiencias');
+    
+    if(loading) return <p>{loading}</p>
+    if(error) return <p>Error : {error}</p>
+
+    return(
+        <section className="experience-section">
+            <h2>Experiencia</h2>
+            <div className="timeline">
+               {/*  {experiences.map()} */}
+            </div>
+        </section>
+    )
 }
 
 export default Experiencias
