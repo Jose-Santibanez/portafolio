1ebb1764872bd8fc86417a1a7f9a6930dd108575
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-11 18:50:51 -0300

    Formateando la fecha de componente itemEducation

--- frontend/portafolio/src/components/common/ItemEducacion.jsx
+++ frontend/portafolio/src/components/common/ItemEducacion.jsx
@@ -1,9 +1,14 @@
+import { useFormattedDate } from "../../hooks/useFormattedDate"
 
 const ItemEducacion = ({props})=>{
+
+    const fechaInicio = useFormattedDate(props.fecha_inicio);
+    const fechaFin = useFormattedDate(props.fecha_fin)
+
     return(
         <div className="timeline-item reveal">
             <h3>{props.titulo} - {props.institucion}</h3>
-            <span>{props.fecha_inicio} - {props.fecha_fin}</span>
+            <span>{fechaInicio} hasta {fechaFin}</span>
             <p>{props.descripcion}</p>
         </div>
     )
