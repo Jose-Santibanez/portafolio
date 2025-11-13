f582098835f6f425bac2dc5fec7ce3c762455b6a
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-12 23:08:06 -0300

    useFormattedDate modificado

--- frontend/portafolio/src/hooks/useFormattedDate.js
+++ frontend/portafolio/src/hooks/useFormattedDate.js
@@ -5,7 +5,7 @@ export const useFormattedDate = (fecha, options = {}) =>{
     const date = new Date(fecha);
 
     const year = date.getFullYear();
-    const month = String(date.getMonth()+1).padStart(2, "0")
+    const month = String(date. getMonth()+1).padStart(2, "0")
 
     return `${month} / ${year}`;
 }
