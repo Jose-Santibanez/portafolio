992a6e1cce3d5e787ba817e84f345285f85c8969
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-14 23:55:09 -0300

    creacion del controller de contacto

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -1,2 +1,17 @@
+import { contacModel } from "../models/contact.model.js";
+import { userModel } from "../models/user.model";
 
-
+const contact = async(req, res) =>{
+    const {nombre,email, mensaje} = req.body;
+    const newContact ={
+        nombre,
+        email,
+        mensaje
+    }
+    try{
+        let contact = await userModel.insertContact(newContact)
+        return res.json(contact)
+    } catch(e){
+        return res.status(500).json({message : "error en el servidor"})
+    }
+}
--- backend/models/contact.model.js
+++ backend/models/contact.model.js
@@ -3,7 +3,7 @@ import { pool } from "../database/connection.js";
 
 const insertContact = async(contacto) =>{
     const query = 'insert into contactos (nombre,email,mensaje) values ($1,$2,$3)';
-    const {rows} = await pool.query(query,[])
+    const {rows} = await pool.query(query,[])  //falta las parametros
     return rows[0]
 } 
 
