058dfe05878999f331f7324751d75f3504eda5cc
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-16 08:56:19 -0300

    Creando middleware para manejar la autenticación con jwt

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = (req, res, next) =>{
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(400).json({message: 'token no encontrado'})
+    }
+
+    try{
+        const decodeToken = jwt.decode(token)
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.token = token;
+        req.user = payload;
+        next()
+    }catch(e){
+        console.log(e);
+        return res.status(401).send({e : 'token invalido'})
+    }
+}
