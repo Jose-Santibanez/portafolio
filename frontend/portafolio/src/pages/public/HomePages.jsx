3381073f0cd266f3b9f56045b22228c6c0043b9e
Jose-Santibanez <j.patricio.rqm@outlook.es>
2025-11-17 22:02:39 -0300

    creación ficheros de login, capeta privada y publica

---
+++ frontend/portafolio/src/pages/public/HomePages.jsx
@@ -0,0 +1,34 @@
+import Navbar from "../../components/common/Navbar"
+import Hero from "../../components/sections/Hero"
+import Habilidades from "../../components/sections/Habilidades"
+import Proyectos from "../../components/sections/Proyectos"
+import Experiencias from "../../components/sections/Experiencias"
+import Educacion from "../../components/sections/Educacion"
+import Contacto from "../../components/sections/Contacto"
+import Footer from "../../components/common/Footer"
+
+const HomePages =()=>{
+
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+            <main>                
+                <Habilidades/>
+                <Proyectos/>
+                <Experiencias/>
+                <Educacion/>
+               
+            </main>
+             <Contacto/>
+            <Footer/>
+            
+        </>
+                        
+    )
+
+
+
+}
+
+export default HomePages
